4119d03c6ed9dee85532dd93cb9d0ca0ced5cc49
Yevhen Larin <minard1usz@gmail.com>
2023-05-22 20:28:49 +0300

    added changes

--- js/hw2.js
+++ js/hw2.js
@@ -1,17 +1,3 @@
-// let numOrStr = prompt('input number or string');
-// console.log(numOrStr)
-
-// if(numOrStr === null) {
-// console.log('ви скасували')
-// } 
-// else if( numOrStr.trim() === '' ) {
-// console.log('Empty String');
-// } else if ( isNaN( +numOrStr ) ) {
-// console.log(' number is Ba_NaN')
-// } else {
-// console.log('OK!')
-// }
-
 let numOrStr = prompt("input number or string");
 switch (isNaN(numOrStr) || numOrStr) {
     case null:
